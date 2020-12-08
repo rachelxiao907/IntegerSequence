6a85f4618d38580a723809c485e683741b4ebd6f
Rachel Xiao <rachelxiao907@gmail.com>
2020-12-08 11:38:24 -0500

    Created constructor

--- ArraySequence.java
+++ ArraySequence.java
@@ -4,5 +4,11 @@ public class ArraySequence implements IntegerSequence{
   private int[] data;
 
   /*Construct the sequence by copying values from the other array into the data array*/
-  public ArraySequence(int [] other){  }
+  public ArraySequence(int[] other) {
+    data = new int[other.length];
+    for (int i = 0; i < other.length; i++) {
+      data[i] = other[i];
+    }
+    currentIndex = 0;
+  }
 }
